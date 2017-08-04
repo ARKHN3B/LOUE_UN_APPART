e2a07bf7c15973e92caedb8b2761d5765e8bc724
Benjamin Lemullois <ARKH.DEVELOPMENT@icloud.com>
2017-08-04 14:29:01 +0200

    Push

---
+++ HTML Concept/script.js
@@ -0,0 +1,11 @@
+// var $header = document.getElementById('header'); //dhiz
+
+// $header.onmouseover = function(){
+//     var $spanheader = document.getElementById('form_head');
+//         $spanheader.style.opacity = 1;
+// }
+
+// $header.onmouseout = function(){
+//     var $spanheader = document.getElementById('form_head');
+//         $spanheader.style.opacity = 0;
+// }
